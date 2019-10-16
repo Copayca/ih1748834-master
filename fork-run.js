55be432bf8e6daf68731eee320873e625f0e9b56
Copayca <56300020+Copayca@users.noreply.github.com>
2019-10-16 17:32:22 +0300

    Delete fork-run.js

--- fork-run.js
+++
@@ -1,5 +0,0 @@
-global.FORK_MODE = 1; //set to 1 for fork
-global.NETWORK = "WBN-MAIN"; //max 10 chars
-global.START_NETWORK_DATE = 1570363400000; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
-global.CONSENSUS_PERIOD_TIME = 2000; //ms
-global.FORK_IP_LIST = [{"ip":"193.124.180.20","port":30000},{"ip":"194.67.221.153","port":30000},{"ip":"81.90.180.31","port":30000},{"ip":"81.90.180.31","port":30030}];
